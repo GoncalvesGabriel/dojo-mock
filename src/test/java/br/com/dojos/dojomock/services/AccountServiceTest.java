13431c7a877c0451e0a8cff733548de0bf512dbd
vitor.alves <vitors.alves@hotmail.com>
2020-05-20 16:59:04 -0300

    Gabtirm

--- src/test/java/br/com/dojos/dojomock/services/AccountServiceTest.java
+++ src/test/java/br/com/dojos/dojomock/services/AccountServiceTest.java
@@ -1,29 +1,26 @@
 package br.com.dojos.dojomock.services;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 import br.com.dojos.dojomock.dto.account.AccountDTO;
 import br.com.dojos.dojomock.dto.account.CreateAccountDTO;
 import org.junit.Test;
-
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 
 public class AccountServiceTest {
 
     @Test
-    public void createAccountSucess(){
+    public void createAccountSucess() {
         CreateAccountDTO createAccountDTO = new CreateAccountDTO();
         createAccountDTO.setDocumentNumber("12345");
 
         AccountService service = new AccountService();
         AccountDTO accountLeirbag = service.createAccount(createAccountDTO);
 
-
-        assertThat(accountLeirbag.getDocumentNumber(),equalTo("12345"));
-        assertThat(accountLeirbag.getId(),);
+        assertThat(accountLeirbag.getDocumentNumber(), equalTo("12345"));
+        assertThat(accountLeirbag.getId(), equalTo(1L));
     }
-
-
 
 
 }
