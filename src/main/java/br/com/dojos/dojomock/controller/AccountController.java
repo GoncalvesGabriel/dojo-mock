1a49c8f28a151cbefb02af8bec9bc3f9225e8e76
vitor.alves <vitors.alves@hotmail.com>
2020-06-17 19:23:15 -0300

    Delete

--- src/main/java/br/com/dojos/dojomock/controller/AccountController.java
+++ src/main/java/br/com/dojos/dojomock/controller/AccountController.java
@@ -8,12 +8,15 @@ import com.sun.jndi.toolkit.url.Uri;
 import java.net.URI;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -41,5 +44,11 @@ public class AccountController {
     public ResponseEntity<AccountDTO> findAccount(@PathVariable long id){
         Optional<AccountDTO> optionalAccount = accountService.findDTOById(id);
         return ResponseEntity.of(optionalAccount);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
     }
 }
--- src/main/java/br/com/dojos/dojomock/services/account/AccountService.java
+++ src/main/java/br/com/dojos/dojomock/services/account/AccountService.java
@@ -48,4 +48,9 @@ public class AccountService {
         }
         return Optional.empty();
     }
+
+
+    public void deleteAccount(Long id) {
+        this.accountRepository.deleteById(id);
+    }
 }
